b80acdd52ff404067d13f7d5203a6c930ecf748f
yusufurur <yusufurur@gmail.com>
2023-03-02 17:23:12 -0500

    Dynamic Tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,72 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetsData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const createTweetElement = function(tweet) {
+  let $tweet = $("<article>").addClass("tweet");
+  const html = `
+    <header>
+      <img class="tweet--avatar" src="${tweet.user.avatars}"> 
+      <h2>${tweet.user.name}</h2>
+      <small>${tweet.user.handle}</small>
+    </header>
+    <div>
+      <p>${tweet.content.text}</p>
+    </div>
+    <hr />
+    <footer>
+      <h2>10 days ago</h2>
+      <div>
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>`;
+  const tweetElement = $tweet.append(html); 
+  return tweetElement;
+};
+
+$(document).ready(function() {
+
+  const tweetsContainer = $("#tweets-container").html("") 
+  for (let tweet of tweetsData) {
+    let tweetElement = createTweetElement(tweet)
+    tweetsContainer.prepend(tweetElement)
+  }
+  // Test / driver code (temporary)
+  // console.log($tweet); // to see what it looks like
+  // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+});
+
+const loadTweets = function() {
+  $.get("/tweets/", function(newTweet) {
+    renderTweets(newTweet.reverse());
+  });
+};
+
+loadTweets();
